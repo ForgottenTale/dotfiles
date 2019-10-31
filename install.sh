4d813b41c581ebf597d6df0b0e9ea9103e45d479
renshuki <renshuki@mail.com>
2019-11-01 01:03:40 +0900

    Update install script

--- install.sh
+++ install.sh
@@ -2,6 +2,20 @@
 
 if [ `uname` == "Linux" ]; then
 	echo "Running on Linux..."
+	
+	# Install necessary packages	
+	sudo apt-get install -y git
+	sudo apt-get install -y stow
+	sudo apt-get install -y zsh
+	sudo apt-get install -y tmux
+
+	# Stow to create symlinks
+	stow zsh
+	stow vim
+	stow tmux
+	stow rbenv
+	stow gem
+
 elif [ `uname` == "Darwin" ]; then
 	echo "Running on OSX..."
 else
