9f2110f0a17121102c2759ccffa88c8bbdef4061
renshuki <renshuki@users.noreply.github.com>
2019-11-05 10:28:14 +0900

    Improve Mac installation script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ `uname` == "Linux" ]; then
 	echo "Running on Linux..."
-	
+
 	# Install necessary packages	
 	sudo apt-get install -y git
 	sudo apt-get install -y vim
@@ -17,8 +17,25 @@ if [ `uname` == "Linux" ]; then
 	stow rbenv
 	stow gem
 
+	# Make ZSH default shell
+	chsh -s $(which zsh)
+
 elif [ `uname` == "Darwin" ]; then
 	echo "Running on OSX..."
+
+	# Install Homebrew
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+	# Install necessary packages
+	brew install git
+	brew intall vim
+	brew install stow
+	brew install zsa
+	brew install tmuxh
+
+	# Make ZSH default shell
+	sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
+
 else
 	echo "OS not detected. Abort!"
 	exit 0
